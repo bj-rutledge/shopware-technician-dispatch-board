99fe33d78201c93644e3102650aaf3f3b2306f2a
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2022-12-12 12:12:52 -0800

    removed draggable section from remove list, as it is likely to be necessary. Prettified code.

--- scripts/removeElements.js
+++ scripts/removeElements.js
@@ -1,35 +1,33 @@
-/**Created by BJ Rutledg 
+/**Created by BJ Rutledg
  * 12/01/2022
- * Licence Apache 
+ * Licence Apache
  */
 
-
-const style = 'display: none;'
+const style = "display: none;";
 
 const iterateNodesChangeStyle = (nodes) => {
-    
-    for(let node in nodes) {
-        const curr = nodes[node];
-        if(typeof curr === "object"){
-            curr.style = style;
-        }
-    };
-}
+  for (let node in nodes) {
+    const curr = nodes[node];
+    if (typeof curr === "object") {
+      curr.style = style;
+    }
+  }
+};
 
 const removeElements = () => {
-    const classes = [
-        'sidebar closed', 
-        'sidebar', //if sidebar is open 
-        'header clearfix',
-        'top-border',
-        'current-shop-breadcrumb',
-        'non-technician-section clearfix',
-        // 'section-seperator draggable'
-    ]
-    
-    classes.forEach(_class => {
-        iterateNodesChangeStyle(document.getElementsByClassName(_class)); 
-    });
-}
+  const classes = [
+    "sidebar closed",
+    "sidebar", //if sidebar is open
+    "header clearfix",
+    "top-border",
+    "current-shop-breadcrumb",
+    "non-technician-section clearfix",
+    // 'section-seperator draggable'
+  ];
+
+  classes.forEach((_class) => {
+    iterateNodesChangeStyle(document.getElementsByClassName(_class));
+  });
+};
 
 removeElements();
